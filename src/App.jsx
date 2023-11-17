df304b368483bff9abb7d4dbc6baeee228b53796
KimKyeongSeog <kimks9241@naver.com>
2023-11-17 09:38:03 +0900

    Header_done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
+import Header from "./components/Header";
 import TopBanner from "./components/TopBanner";
 
 const App = () => {
   return (
-    <>
+    <div className="font-pretendard text-techit-gray-300">
       <TopBanner />
-      <header className="bg-red-100 h-16"></header>
+      <Header />
       <div className="bg-blue-100 h-[400px]">Banner</div>
-      <section className="mt 20 bg-green-100 max-w-screen-xl mx-auto px-6">
+      <section className="mt-20 bg-green-100 max-w-screen-xl mx-auto px-6">
         <div className="bg-red-100 h-[64.5px]">KDT</div>
-        <div className="bg-purple-100 grid grid-cols-4 gap-6 justify-items-start">
+        <div className="bg-purple-100 grid grid-cols-4 gap-6 justify-items-center">
           <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
           <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
           <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
           <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
           <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
           <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
-          <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>  
-          <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div> 
+          <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-yellow-100 w-[290px] h-[416px]">Card</div>
         </div>
       </section>
+      <section className="mt-20 bg-green-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-red-100 h-[64.5px]">Schedule</div>
+        <div className="bg-blue-100 h-[280px]">KDT</div>
+      </section>
+      <div className="mt-20 bg-yellow-100 h-[206px]">Alarm Banner</div>
       <section className="mt-20 bg-green-100 max-w-screen-xl mx-auto px-6">
         <div className="bg-red-100 h-[64.5px]">Onboarding</div>
         <div className="bg-purple-100 grid grid-cols-3 gap-6 justify-items-center">
@@ -37,11 +45,10 @@ const App = () => {
           <div className="bg-yellow-100 w-[604px] h-[310px]">Card</div>
         </div>
       </section>
-      <footer className="mt-`40 max-w-screen-xl mx-auto bg-gray-100 h-[418px]">
+      <footer className="mt-40 max-w-screen-xl mx-auto bg-gray-100 h-[418px]">
         Footer
       </footer>
-    </>
-
+    </div>
   );
 };
 
